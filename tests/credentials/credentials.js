528fac706e5eded18425eff563b58ea39ca31506
Minja Marcetic <46632188+minjawork@users.noreply.github.com>
2019-12-11 14:44:04 +0100

    Update credentials.js

--- tests/credentials/credentials.js
+++ tests/credentials/credentials.js
@@ -1,6 +1,6 @@
 class Credentials {
-  get username () { return ('aqa@payever.org'); }
-  get password () { return ('Aqacool123!'); }
+  get username () { return ('username'); }
+  get password () { return ('password'); }
   get homePageUrl () { return ('https://commerceos.staging.devpayever.com/business/2c965cbc-df55-4785-b820-cd803486f8d7/info/overview'); }
   get shopUrl () { return ('https://commerceos.staging.devpayever.com/business/2c965cbc-df55-4785-b820-cd803486f8d7/builder/shop/6c082a7a-5446-4f25-a3bd-9d080cf54921/builder/editor/home?themeType=application&theme=6c082a7a-5446-4f25-a3bd-9d080cf54921&channelSet=a644f50e-b992-433a-950e-c66dc247c80e&backUrl=&getBusinessThemeByAppTheme=true'); }
 }
